ed41280dad0244d232098d54dea6b32a4749ae68
Ari Vaniderstine <hack@ariari.io>
2017-01-02 20:55:05 +1100

    Initial Commit

---
+++ LoL Friends/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LoL Friends
+//
+//  Created by Arielle Vaniderstine on 2017-01-02.
+//  Copyright © 2017 Citron Digital. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
